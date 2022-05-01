f87c6628435f35c88fcd8a825c72b908fff9ff79
Nischal Babu Bohara <nischalbohara77@gmail.com>
2022-05-01 13:36:22 +0545

    jsd

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     extends: [
         "@commitlint/config-conventional"
-
     ],
     rules: {
         "type-enum": [2, 'always', ['Feature', 'BugFix', 'StyleChange', 'Refactoring', 'Docs', 'Improvement', 'ConfigurationChanges', 'Test', 'Chore', 'Revert']],
